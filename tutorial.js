566910bdcc6f5346ebf71f9f5b1c0cf2797de00c
Tyler Hallada <tyler@hallada.net>
2017-02-19 01:52:53 -0500

    Initial tutorial setup

---
+++ tutorial.js
@@ -0,0 +1,16 @@
+//Create the renderer
+var renderer = PIXI.autoDetectRenderer(256, 256);
+
+//Add the canvas to the HTML document
+document.body.appendChild(renderer.view);
+
+//Create a container object called the `stage`
+var stage = new PIXI.Container();
+
+//Tell the `renderer` to `render` the `stage`
+renderer.render(stage);
+
+renderer.view.style.position = "absolute";
+renderer.view.style.display = "block";
+renderer.autoResize = true;
+renderer.resize(window.innerWidth, window.innerHeight);
